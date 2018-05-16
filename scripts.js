5d7d78743b94dbfd26af4c894fb3f99db3e2162f
emachaffie <emachaffie@gmail.com>
2018-05-16 13:41:57 -0400

    Started outlining functions in js and skeleton HTML

---
+++ scripts.js
@@ -0,0 +1,43 @@
+import request from 'superagent'
+
+let notesList = []
+
+// Function to delete a note
+function deleteNote (noteId) {
+  request
+    .delete(https://notes-api.glitch.me/api/notes/notes/`${noteId}`)
+    .auth('liz', 'dogsarebetterthancats')
+    .then(response => {
+      notesList = notesListfilter(note => note._id !== noteId)
+      updateAllNotes(notesList)
+    })
+}
+
+// Function to edit a note
+
+// Function to add new note, needs to be event listener on submit button
+document.getElementById('AddNewNoteForm').addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  const title = document.getElementById('noteTitle').value
+  const noteBody = document.getElementById('noteBody').value.split(/\s*,\s*/)
+  const tags = document.getElementById('tags').value
+
+  request
+    .post(https://notes-api.glitch.me/api/notes)
+    .auth('liz', 'dogsarebetterthancats')
+    .send({
+      noteTitle: title,
+      noteBody: authors,
+      tags: tags
+    })
+    .then(response => {
+      document.getElementById('addNewNoteForm').reset()
+      document.getElementById('addNewNoteForm').classList.add('hidden')
+      document.getElementById('submitButton').classList.remove('hidden')
+      noteList.push(response.body)
+      updateNotes(noteList)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
